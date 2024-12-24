a4fbaecc9e241ebadb02c68e5492d4be171fa93d
Daouda SEYE <97038478+kryptonic21@users.noreply.github.com>
2024-12-24 14:24:35 +0000

    dummy datta 6

--- config/firebase.js
+++ config/firebase.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const path = require("path");
 console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 // Importer votre clé privée JSON téléchargée depuis Firebase Console
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-// const serviceAccount = require(path.join(
-// __dirname,
-// process.env.GOOGLE_APPLICATION_CREDENTIALS
-// ));
+// const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const serviceAccount = require(path.join(
+  __dirname,
+  process.env.GOOGLE_APPLICATION_CREDENTIALS
+));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
