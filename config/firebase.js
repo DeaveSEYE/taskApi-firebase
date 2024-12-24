56fa76df65502dc585f84c685d70f78aa4e579eb
Daouda SEYE <97038478+kryptonic21@users.noreply.github.com>
2024-12-24 15:04:57 +0000

    dummy datta vercel

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,19 @@
 const admin = require("firebase-admin");
-require("dotenv").config();
-const path = require("path");
-console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-// Importer votre clé privée JSON téléchargée depuis Firebase Console
-// const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-const serviceAccount = require(path.join(
-  __dirname,
-  process.env.GOOGLE_APPLICATION_CREDENTIALS
-));
+// require("dotenv").config();
+// Charger les variables d'environnement depuis Vercel
+const serviceAccount = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
